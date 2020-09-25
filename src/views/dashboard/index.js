e423cdb2925619b240e0ab5303c269550f24ec50
Hammad-23 <62185607+Hammad-23@users.noreply.github.com>
2020-09-25 18:52:25 +0500

    1dash done

--- src/views/dashboard/index.js
+++ src/views/dashboard/index.js
@@ -27,10 +27,16 @@ function DashboardCompany(){
             </Col>
         </Row>
         {/* navbar ends */}
-        <Row>
+
+
+        <Row style={{display: 'flex', justifyContent: 'space-around', alignItems: 'center', marginTop: '6rem'}}>
+
+            {/* first table starts */}
             <Col>
 
-                <table border='1'>
+               <h1>Employees Data</h1>
+
+                <table style={{height: '20rem', width:'30rem'}} border='1'>
                     <tr>
                         <th>Name</th>
                         <th>Position</th>
@@ -44,12 +50,101 @@ function DashboardCompany(){
                         <td>100000</td>
                         <td>25-02-2019</td>
                     </tr>
+                    <tr>
+                        <td>Hammad</td>
+                        <td>Software Engineer</td>
+                        <td>100000</td>
+                        <td>25-02-2019</td>
+                    </tr>
+                    <tr>
+                        <td>Hammad</td>
+                        <td>Software Engineer</td>
+                        <td>100000</td>
+                        <td>25-02-2019</td>
+                    </tr>
 
 
                 </table>
+                <br/>
+
+                <button 
+
+                style={{
+
+                    backgroundColor: 'rgb(32, 46, 46)',
+                    color: 'white',
+                    borderRadius: '10px',
+                    width:'8rem'
+
+
+                }}
+                
+                >Add Employee</button>
             
             </Col>
+            {/* first table ends */}
+            
+
+            {/* second table starts */}
+            <Col>
+              <h1>Students Data</h1>           
+ 
+               <table style={{height: '20rem', width:'30rem'}} border='1'>
+                  <tr>
+                      <th>Name</th>
+                      <th>Class</th>
+                       <th>Number</th>
+                      <th>Age</th>
+        
+                 </tr>
+                 <tr>
+                     <td>Saad</td>
+                     <td>FC-MS 01</td>
+                     <td>03127865655</td>
+                     <td>22</td>
+                </tr>
+                <tr>
+                     <td>Saad</td>
+                     <td>FC-MS 01</td>
+                     <td>03127865655</td>
+                     <td>22</td>
+                </tr>
+                <tr>
+                     <td>Saad</td>
+                     <td>FC-MS 01</td>
+                     <td>03127865655</td>
+                     <td>22</td>
+                </tr>
+               
+
+
+              </table>
+              <br/>
+              <button 
+
+style={{
+
+    backgroundColor: 'rgb(32, 46, 46)',
+    color: 'white',
+    borderRadius: '10px',
+    width:'8rem'
+
+
+}}
+
+>Add Student</button>
+
+          </Col>
+          {/* second table ends */}
+
+
+
         </Row>
+
+
+        <br/><br/>
+
+        
 
 
         </>
